32a8dc881fa5f812fbbe5db641c5a2cb2632f253
nomuyoshi <nomuyoshi0111@gmail.com>
2021-06-27 00:30:30 +0900

    [chat]テンプレートを使う

--- chat/main.go
+++ chat/main.go
@@ -1,23 +1,36 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
 )
 
-func main() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		rw.Write([]byte(`
-			<html>
-			  <head>
-			    <title>チャット</title>
-			  </head>
-			  <body>
-			    チャットしよう
-			  </body>
-			</html>
-		`))
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTP はHTTPリクエストを処理する
+// インターフェース Handler を実装
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// テンプレートのコンパイル
+	// 1度コンパイルすれば良いのでsync.Onceを使っている
+	// sync.Onceなら複数のゴルーチンからServeHTTPが呼ばれても1度しか実行されない
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
+
+	if err := t.templ.Execute(w, nil); err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe: ", err)

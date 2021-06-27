0289d6f2cc6e3d860d278c871ce1f9049a1cc6da
nomuyoshi <nomuyoshi0111@gmail.com>
2021-06-27 16:26:25 +0900

    [chat] /room ハンドラー  # Please enter the commit message for your changes. Lines starting

--- chat/room.go
+++ chat/room.go
@@ -1,11 +1,27 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
 // room はクライアントとの接続管理、メッセージ受付を行う
 type room struct {
 	forward chan []byte      // ルーム内にいるクライアントに転送するメッセージを保持するチャネル
 	join    chan *client     // ルームに入室しようとしているクライアント管理のチャネル
 	leave   chan *client     // ルームから退室しようとしているクライアント管理のチャネル
 	clients map[*client]bool // ルームに入室中のクライアント管理
+}
+
+func newRoom() *room {
+	return &room{
+		forward: make(chan []byte),
+		join:    make(chan *client),
+		leave:   make(chan *client),
+		clients: make(map[*client]bool),
+	}
 }
 
 func (r *room) run() {
@@ -34,4 +50,32 @@ func (r *room) run() {
 			}
 		}
 	}
+}
+
+const (
+	socketBufferSize  = 1024
+	messageBufferSize = 256
+)
+
+var upgrader = &websocket.Upgrader{
+	ReadBufferSize:  socketBufferSize,
+	WriteBufferSize: socketBufferSize,
+}
+
+func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	socket, err := upgrader.Upgrade(w, req, nil)
+	if err != nil {
+		log.Fatal("ServeHTTP: ", err)
+		return
+	}
+	client := &client{
+		socket: socket,
+		send:   make(chan []byte, messageBufferSize),
+		room:   r,
+	}
+	r.join <- client
+	defer func() { r.leave <- client }()
+
+	go client.write()
+	client.read()
 }

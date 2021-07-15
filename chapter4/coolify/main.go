e7b03524f649eb4d756537129f3fa79bcd5d124c
nomuyoshi <nomuyoshi0111@gmail.com>
2021-07-16 07:07:35 +0900

    ch4.2 coolify

---
+++ chapter4/coolify/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"time"
+)
+
+func randBool() bool {
+	return rand.Intn(2) == 0
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	s := bufio.NewScanner(os.Stdin)
+	for s.Scan() {
+		word := []byte(s.Text())
+		if !randBool() {
+			fmt.Println(string(word))
+			return
+		}
+		var vI int = -1
+		for i, char := range word {
+			switch char {
+			case 'a', 'i', 'u', 'e', 'o', 'A', 'I', 'U', 'E', 'O':
+				if randBool() {
+					vI = i
+				}
+			}
+		}
+		if vI >= 0 {
+			if randBool() {
+				word = duplicateVowel(word, vI)
+			} else {
+				word = removeVowel(word, vI)
+			}
+		}
+		fmt.Println(string(word))
+	}
+}
+
+func duplicateVowel(word []byte, i int) []byte {
+	return append(word[:i+1], word[i:]...)
+}
+
+func removeVowel(word []byte, i int) []byte {
+	return append(word[:i], word[i+1:]...)
+}
